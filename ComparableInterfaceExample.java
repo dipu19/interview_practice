ed70889044e4c4b7e62c8dc006fde0f972419552
dipu19 <50631686+dipu19@users.noreply.github.com>
2020-12-18 08:08:53 +0530

    Comprable Interface Example

---
+++ ComparableInterfaceExample.java
@@ -0,0 +1,39 @@
+package com.practicecode;
+
+import java.util.Comparator;
+import java.util.NavigableMap;
+import java.util.TreeSet;
+
+public class ComparableInterfaceExample {
+
+	public static void main (String[] args)
+	{
+		TreeSet<Integer> treeSet = new TreeSet<Integer>( new MyComparator());
+		treeSet.add(10);
+		treeSet.add(5);
+		treeSet.add(12);
+		treeSet.add(8);
+		treeSet.add(0);
+	}
+}
+
+class MyComparator implements Comparator
+{
+  public int compare(Object obj1,Object obj2)
+  {
+	  int i1 = (Integer)obj1;
+	  int i2 = (Integer)obj2;
+	  if(i1<i2)
+	  {
+		  return -1;
+	  }
+	  if(i1>i2)
+	  {
+		  return 1;
+	  }
+	  return 0;
+  
+  }
+
+
+}
