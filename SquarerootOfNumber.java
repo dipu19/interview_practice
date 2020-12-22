5aa5e084e50b5af53de0ce519eb4aad6846e6cb5
dipu19 <50631686+dipu19@users.noreply.github.com>
2020-12-22 08:38:38 +0530

    Square root of a number using math function

---
+++ SquarerootOfNumber.java
@@ -0,0 +1,11 @@
+package com.practicecode;
+
+public class SquarerootOfNumber {
+
+	public static void main(String[] args)
+	{
+		int number = 16;
+		double sqrt = Math.sqrt((double)number);
+		System.out.println(sqrt);
+	}
+}
